5cf0d7d730de96900b5750f73cc83d74852a0c75
Patryk Zimny <patryk.zim98@gmail.com>
2021-07-20 14:04:58 +0200

    Changes in fragments (HomeFragment) 0.2 Added horizontal recycler view.

--- app/src/main/java/com/example/fragmentdemo/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/fragmentdemo/RecyclerViewAdapter.kt
@@ -10,9 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.fragmentdemo.databinding.CardViewBinding
 import com.example.fragmentdemo.databinding.FragmentHomeBinding
 
-class RecyclerViewAdapter(private val data: List<DataObject>): RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
+class RecyclerViewAdapter(
+    private val data: List<DataObject>
+    ): RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
 
-    //private lateinit var binding: CardViewBinding
+    private lateinit var binding: CardViewBinding
     private val items: MutableList<CardView>
 
     init{
@@ -20,21 +22,28 @@ class RecyclerViewAdapter(private val data: List<DataObject>): RecyclerView.Adap
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_view, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view, parent, false)
 
         return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.tvTitle.text = items[position]
+        holder.tvTitle.text = data[position].title
+
+        items.add(holder.cardItem)
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-    inner class MyViewHolder internal constructor(itemView: View): RecyclerView.ViewHolder(itemView){
+    inner class MyViewHolder
+    internal constructor(
+        itemView: View
+    ): RecyclerView.ViewHolder(itemView){
         val tvTitle: TextView = itemView.findViewById(R.id.tvCard)
+//        val tvTitle: TextView =
         val cardItem: CardView = itemView.findViewById(R.id.card_item)
     }
 
--- app/src/main/java/com/example/fragmentdemo/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fragmentdemo/fragments/HomeFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fragmentdemo.DataObject
 import com.example.fragmentdemo.R
+import com.example.fragmentdemo.RecyclerViewAdapter
 import com.example.fragmentdemo.databinding.ActivityMainBinding
 import com.example.fragmentdemo.databinding.FragmentHomeBinding
 
@@ -20,18 +23,27 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val recyclerHorizontalView = view.findViewById<RecyclerView>(R.id.horizontal_rv_home)
+
         val data: MutableList<DataObject> = ArrayList()
         for (i in 1..10)
-            data.add(DataObject("Test $i"))
+            data.add(DataObject("Trip $i"))
 
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
 
-        //binding = FragmentHomeBinding.inflate(layoutInflater)
+        val adapter = RecyclerViewAdapter(data)
 
-        binding.horizontalRvHome.layoutManager = LinearLayoutManager(context)
-        binding.horizontalRvHome.adapter
+//        binding.horizontalRvHome.layoutManager = layoutManager
+//        binding.horizontalRvHome.setHasFixedSize(true)
+//        binding.horizontalRvHome.adapter = adapter
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        recyclerHorizontalView.layoutManager = layoutManager
+        recyclerHorizontalView.setHasFixedSize(true)
+        recyclerHorizontalView.adapter = adapter
+
+
+        return view
     }
 
 
